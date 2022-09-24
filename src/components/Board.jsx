3cbb92afe3ebe7eb22cb4d851510d97589566072
ShreyaMBelanekar42 <shreyaluvbelanekar@gmail.com>
2022-09-24 15:27:33 +0530

    calculate winner state

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,32 +1,13 @@
 import React, {useState} from 'react'
 import Square from './Square'
 
-const Board = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
+const Board = ({board, handleSquareClick}) => {
 
-  const handleSquareClick = position => {
-    if (board[position]){
-      return;
-    }
-
-    setBoard(prev => {
-      return prev.map((square, pos) => {
-        if (pos === position) {
-          return isXNext ? 'X' : 'O';
-        }
-        return square;
-      });
-    });
-  
-    setIsXNext(prev => !prev);
+  const renderSquare = position => {
+    return(
+  <Square value={board[position]} onClick={() => handleSquareClick(position)}/>
+    );
   };
-
-const renderSquare = position => {
-  return(
-<Square value={board[position]} onClick={() => handleSquareClick(position)}/>
-  );
-};
 
   return (
     <div className='board'>
